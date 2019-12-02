b4945d26d5619e039ab23930f27c5999ef5cf341
priyesh.diukar <priyesh.diukar@siemens.com>
2019-12-02 17:32:25 +0530

    Add zuul api service implemeatation

--- currency-conversion-service/currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -5,11 +5,16 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service")
+/* Do not connect to currency-exchange-service directly.
+   Instead connect via the netflix-zuul-api-gateway-server.
+ */
+// @FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    // @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
     
 }
