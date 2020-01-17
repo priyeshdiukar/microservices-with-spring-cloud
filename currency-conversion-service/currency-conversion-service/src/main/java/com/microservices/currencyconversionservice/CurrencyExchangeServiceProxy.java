47e6b304c88611aef30e5990723428c64d164b24
priyesh.diukar <priyesh.diukar@siemens.com>
2020-01-17 16:47:45 +0530

    enhance comments, add a batch file

--- currency-conversion-service/currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 /* Do not connect to currency-exchange-service directly.
    Instead connect via the netflix-zuul-api-gateway-server.
  */
-// @FeignClient(name="currency-exchange-service")
+// @FeignClient(name="currency-exchange-service", url="localhost:8000")
 @FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
---
+++ devLauncher.bat
@@ -0,0 +1,7 @@
+@echo off
+start /B "" code currency-conversion-service 
+start /B "" code currency-exchange-service
+start /B "" code netflix-eureka-naming-server
+start /B "" code netflix-zuul-api-gateway-server
+start /B "" code spring-cloud-config-server
+exit
